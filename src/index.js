ac904f178c84cea4e89e27f78cdba6d2b70fbec2
Enkoff <0639818413@ukr.net>
2020-10-19 11:12:28 +0300

    fix: change function twelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return [];
+  }
+  let res = [];  
+  for (let i = 0; i < matrix.length; i++) {
+  if (i % 2 !== 0) {
+    res = res.concat(matrix[i].reverse());    
+  } else {
+    res = res.concat(matrix[i]);
+  }
 }
+return res;
+}
+
